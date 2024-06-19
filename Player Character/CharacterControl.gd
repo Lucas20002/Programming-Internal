daf4c9331f391b3c8fada19bfb684cdd4e29391c
Lucas20002 <20002@bayfield-high.school.nz>
2024-06-20 09:07:53 +1200

    Player Character Movement
    Player movement has been implemented and the Test Level is now playable

--- Player Character/CharacterControl.gd
+++ Player Character/CharacterControl.gd
@@ -1,22 +1,37 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
+var can_jump = true
 
+func jump():
+	velocity.y = -JUMP_VELOCITY
+	can_jump = false
+
+func jump_cut():
+	if velocity.y < -400:
+		velocity.y = -400
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	if can_jump == false and is_on_floor():
+		can_jump = true
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_released("ui_up") and can_jump == true:
 		velocity.y = JUMP_VELOCITY
-
+	
+	if Input.is_action_just_released("ui_up"):
+		jump_cut()
+		can_jump = false
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
