c17edf76974a7ac3e837226bd4a53e87ce39b038
Lucas20002 <20002@bayfield-high.school.nz>
2024-07-04 09:34:14 +1200

    Wall Jump Mechanic completed
    wall jump feature works, still need to fix the issue with the player scaling up one wall instead of wall jumping

--- Player Character/CharacterControl.gd
+++ Player Character/CharacterControl.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 const WALL_SPEED_GRAVITY = 100.0
 const SPEED = 400.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 const FLOAT_UP_VELOCITY = -200.0
+const MAX_FLOAT_TIME = 0.5
+
+var float_up_time = 0
 var can_jump = true
 var wall_sliding = false
 var direction_true = true
@@ -13,11 +16,19 @@ func jump():
 	velocity.y = -JUMP_VELOCITY
 	can_jump = false
 
+func _on_coyote_timer_timeout():
+	can_jump = false
+
 func jump_cut():
-	if velocity.y < -400:
-		velocity.y = -400
+	if velocity.y < -600:
+		velocity.y = -600
+
+func _on_wall_jump_timer_timeout():
+	direction_true = true
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 
 func _physics_process(delta):
@@ -29,12 +40,18 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	
 	# Handle jump.
-	if Input.is_action_just_released("jump") and can_jump == true:
+	if Input.is_action_just_released("jump") and can_jump:
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_released("jump") and is_on_wall_only():
+		can_jump = false
 	
 	if Input.is_action_just_released("jump"):
 		jump_cut()
 		can_jump = false
+	
+	if (is_on_floor_only() == false) and can_jump and $CoyoteTimer.is_stopped():
+		$CoyoteTimer.start()
 	
 	if is_on_wall_only():
 		if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
@@ -59,18 +76,20 @@ func _physics_process(delta):
 		float_upwards = true
 		$wallJumpTimer.start()
 	
-	if Input.is_action_pressed("jump") and float_upwards:
+	if Input.is_action_pressed("jump") and float_up_time < MAX_FLOAT_TIME:
 		velocity.y = -400
-		$floatUpTimer.start()
+		float_up_time * delta
+	else:
+		float_up_time = MAX_FLOAT_TIME
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+	if direction_true:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 
-func _on_wall_jump_timer_timeout():
-	direction_true = false
-func _on_float_up_timer_timeout():
-	float_upwards= false
+
+
