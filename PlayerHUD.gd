035318e3b7756e17ed2151b5f13d761f969ae15d
Lucas20002 <20002@bayfield-high.school.nz>
2024-08-01 09:31:50 +1200

    Level Complete screen
    still wip but when it's finished, when the player reaches the end goal it will display Level_complete.tscn and then eventually show the leaderboard

--- PlayerHUD.gd
+++ PlayerHUD.gd
@@ -7,10 +7,21 @@ var start_level_msec = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_level_msec = Time.get_ticks_msec()
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	level_time = Time.get_ticks_msec() - start_level_msec
 	level_time_label.text = str(level_time / 1000.0)
+
+func write_score_to_file():
+	var file = FileAccess.open("score file here", FileAccess.WRITE)
+	print() #(end level time.score)
+	file.seek_end()
+	file.store_string("player name here: " + str()) #str(end level time.score)
+
+func read_high_score():
+	var content = ""
+	if FileAccess.file_exists("score file here"):
+		var file = FileAccess.open("score file here", FileAccess.READ)
+		content = "high score: 0"
+		#timer_scores.txt = content
